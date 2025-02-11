4308d6706837d6b0d9c852f2e4d51387fea8854d
Mikeyjpk <mikey.kerr@truescope.com>
2025-02-11 18:11:27 +1100

    remove t&c routes

--- app/(routes)/Privacy/page.tsx
+++
@@ -1,9 +0,0 @@
-const PrivacyPage = () => {
-	return (
-		<div className="w-full h-screen flex justify-center items-center text-white">
-			Privacy Page content goes here
-		</div>
-	);
-};
-
-export default PrivacyPage;
--- app/(routes)/Terms/page.tsx
+++
@@ -1,9 +0,0 @@
-const TermsPage = () => {
-	return (
-		<div className="w-full h-screen flex justify-center items-center text-white">
-			Terms & Conditions go here
-		</div>
-	);
-};
-
-export default TermsPage;
